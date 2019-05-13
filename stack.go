2bdc260041d764d4c8438d35e6924e345be46b42
Enes Çakır <enes@cakir.web.tr>
2019-05-14 02:09:49 +0300

    Rename stack receivers to s for convention

--- stack.go
+++ stack.go
@@ -20,30 +20,30 @@ func newStack() *stack {
 }
 
 // len returns the number of runes in the stack.
-func (stack *stack) len() int {
-	return stack.size
+func (s *stack) len() int {
+	return s.size
 }
 
 // push adds given rune to end of the stack.
-func (stack *stack) push(value rune) {
-	stack.top = &element{value, stack.top}
-	stack.size++
+func (s *stack) push(value rune) {
+	s.top = &element{value, s.top}
+	s.size++
 }
 
 // pop remove the last element of stack and returns it.
-func (stack *stack) pop() rune {
-	if stack.isEmpty() {
+func (s *stack) pop() rune {
+	if s.isEmpty() {
 		return rune(0)
 	}
 
-	value := stack.top.value
-	stack.top = stack.top.next
-	stack.size--
+	value := s.top.value
+	s.top = s.top.next
+	s.size--
 
 	return value
 }
 
 // isEmpty checks the length of stack is zero or not.
-func (stack *stack) isEmpty() bool {
-	return stack.len() == 0
+func (s *stack) isEmpty() bool {
+	return s.len() == 0
 }
